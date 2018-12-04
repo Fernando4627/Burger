efc113e1a639fd4b57e313a5fb8757c2b23f693f
Fernando Aguilar <fernandoaguilar17@gmail.com>
2018-12-04 16:59:53 -0700

    still not working

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,7 +4,7 @@ let router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.all(function(err, data) {
     var hbsObject = {
       burgers: data
     };
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(err, result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
@@ -30,7 +30,7 @@ router.put("/api/burgers/:id", function(req, res) {
       devoured: req.body.devoured
     },
     condition,
-    function(result) {
+    function(err, result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
